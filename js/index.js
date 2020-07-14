a95fa75d2b1bcf2392cb6655c894a0647e7fb0ae
kafarzada <52622175+kafarzada@users.noreply.github.com>
2020-07-15 02:15:04 +0700

    добавлен mp3 звук)

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const warningAlert = document.getElementById('warningAlert');
 const mainElem = document.querySelector('.main');
 const token  = "&appid=0bd5d2047c05c02794b83b1d2719fae2";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=`;
-
+const song = new Audio('mp3/ring.m4a')
 
 
 
@@ -13,19 +13,15 @@ elemcityname.addEventListener('keypress', function(e) {
 })
 btnSubmit.addEventListener('click', function(event) {
     event.preventDefault()
-    
+    const day = new Date().getDay();
     if(elemcityname.value !== '') {
         fetch(url + elemcityname.value + token)
             .then(response => response.json())
             .then(data => {
-                const day = new Date().getDay();
+                song.play()
                 const temp = Math.round(Number(data['main']['temp']) - 273.15);
                 const weatherCard ="<div class='card-body itemcard'><h3 class='card-title'>Город:" + data['name'] + "</h3><h4 class='card-subtitle mb-2 text-muted'>" + getDay(day)+ "</h4> <div class='card-text'><span class='bold'>Температура</span>: " + temp + "С&#176.</div><div class='card-text'><span class='bold'><span class='bold'>Влажность:</span> " + data['main']['humidity'] + "%</div><span class='bold'>Скорость Ветра:</span> " + data['wind']['speed'] + "m/s</div></div>";
                 mainElem.insertAdjacentHTML('afterbegin', weatherCard)
-
-              
-              
-              console.log(data)
             })
             .catch(error => console.log(error))
     } else {
